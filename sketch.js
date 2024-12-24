b328f333c537879511a476f656cf83b9b8c11c96
Jacqueline Kong <jek2179@columbia.edu>
2024-12-23 22:58:58 -0800

    Move face to center

--- sketch.js
+++ sketch.js
@@ -1,46 +1,65 @@
+let middleX;
+let middleY;
+const mouseXFromMiddle = () => {
+  const distance = mouseX - middleX;
+  return distance <= middleY ? distance : middleY;
+};
+
+const mouseYFromMiddle = () => {
+  const distance = mouseY - middleY;
+  return distance <= middleY ? distance : middleY;
+};
+
 function setup() {
+  middleX = windowWidth / 2;
+  middleY = windowHeight / 2;
   createCanvas(windowWidth, windowHeight);
   background("#93cff9");
 }
 
-const origin = 200;
-const mouseXFromOrigin = () => {
-  const distance = mouseX - origin;
-  return distance <= 200 ? distance : 200;
-};
-
-const mouseYFromOrigin = () => {
-  const distance = mouseY - origin;
-  return distance <= 200 ? distance : 200;
-};
-
-const percentAway = (distance) => {
-  return distance / 200;
-};
-
 function draw() {
   // Head
   fill("#f9d977");
-  circle(200, 200, 200);
+  circle(middleX, middleY, 200);
+
+  const leftEyeX = middleX - 30;
+  const leftEyeY = middleY - 30;
+  const rightEyeX = middleX + 35;
+  const rightEyeY = middleY - 27;
 
   if (mouseIsPressed) {
     // Eyeballs
     fill(255, 255, 255);
-    circle(170, 170, 60);
-    circle(235, 173, 60);
+    circle(leftEyeX, leftEyeY, 60);
+    circle(rightEyeX, rightEyeY, 60);
     fill("black");
-    ellipse(200, 250, 70, 70);
+    ellipse(middleX, middleY + 50, 70, 70);
   } else {
     // Eyeballs
     fill(255, 255, 255);
-    circle(170, 170, 51);
-    circle(235, 173, 40);
+    circle(leftEyeX, leftEyeY, 51);
+    circle(rightEyeX, rightEyeY, 40);
+    // Mouth
     fill("black");
-    ellipse(202, 240, 60, 30);
+    ellipse(middleX + 2, middleY + 40, 60, 30);
   }
 
   // Pupils
   fill("black");
-  circle(170 + mouseXFromOrigin() / 20, 170 + mouseYFromOrigin() / 20, 15);
-  circle(235 + mouseXFromOrigin() / 25, 173 + mouseYFromOrigin() / 25, 15);
+  circle(
+    leftEyeX + mouseXFromMiddle() / 20,
+    leftEyeY + mouseYFromMiddle() / 20,
+    15
+  );
+  circle(
+    rightEyeX + mouseXFromMiddle() / 25,
+    rightEyeY + mouseYFromMiddle() / 25,
+    15
+  );
+}
+
+function windowResized() {
+  middleX = windowWidth / 2;
+  middleY = windowHeight / 2;
+  resizeCanvas(windowWidth, windowHeight);
 }
